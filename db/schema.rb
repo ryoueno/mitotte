7262dbba8262704008ea406ba94bee3553ed33f2
uenoryo <r.r.ag0611@outlook.jp>
2017-05-25 21:40:35 +0900

    Add column uuid to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525113017) do
+ActiveRecord::Schema.define(version: 20170525113540) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.string   "email",                  default: "", null: false
+    t.string   "uuid"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
