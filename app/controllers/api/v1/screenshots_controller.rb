d119335a22a2c9abf206ce23a32b863f1a92f8f8
uenoryo <r.r.ag0611@outlook.jp>
2017-05-26 10:55:16 +0900

    Implement API for uploading screenshots.

---
+++ app/controllers/api/v1/screenshots_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::ScreenshotsController < ApplicationController
+  protect_from_forgery except: :create
+  def create
+    file = fileupload_params[:file]
+    image = file.read
+    src = Digest::MD5.hexdigest(image)
+    extension = File.extname(file.original_filename).strip.downcase[1..-1]
+    output_path = Rails.root.join(App::Application.config.screenshots_path, "#{src}.#{extension}")
+
+    File.open(output_path, 'w+b') do |fp|
+      fp.write image
+    end
+
+    @screenshot = Screenshot.new(:uuid => fileupload_params[:uuid], :src => src, :extension => extension)
+
+    if @screenshot.save
+      render json: @screenshot, status: :created
+    else
+      render json: @screenshot.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def fileupload_params
+    params.require(:screenshot).permit(:uuid, :file)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
   devise_for :users
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # ForAPI
+  # User
   get '/api/v1/users' => 'api/v1/users#index'
+
+  # Screenshot
+  post '/api/v1/screenshots' => 'api/v1/screenshots#create'
 end
