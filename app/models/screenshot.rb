8e3a5cc7d39117b43f116ea674614913f9acdf33
uenoryo <r.r.ag0611@outlook.jp>
2017-05-29 23:59:56 +0900

    Add method name & path to screenshots.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,2 +1,9 @@
 class Screenshot < ApplicationRecord
+  def name
+    self.src + "." + self.extension
+  end
+
+  def path
+    Rails.root.to_s + "/public/images/screenshots/" + self.name
+  end
 end
