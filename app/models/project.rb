ece296182635b50fc5ed83bb11c4345cc8b72081
uenoryo <r.r.ag0611@outlook.jp>
2017-06-07 13:57:25 +0900

    Implement tasks belongs to project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  has_many :tasks
   validates :subject, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { maximum: 200 }
 end
