ad30b3cf9437cf75fb6520b18b968efec00adb4c
uenoryo <r.r.ag0611@outlook.jp>
2017-06-26 19:25:40 +0900

    Add method to screenshot to get recent one.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -8,4 +8,9 @@ class Screenshot < ApplicationRecord
   def path
     Rails.root.to_s + "/public/images/screenshots/" + self.name
   end
+
+  # 5分前 ~ 1秒前 のスクリーンショットを1件取得
+  def recent
+    Screenshot.where(created_at: (self.created_at - 5.minute)..(self.created_at - 1.second)).order("created_at desc").first
+  end
 end
