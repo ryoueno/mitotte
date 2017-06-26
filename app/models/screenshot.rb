55f731b2112ab189231ba222072570ed2485d782
uenoryo <r.r.ag0611@outlook.jp>
2017-06-26 21:11:32 +0900

    Add method similar? to screenshot.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,6 +1,12 @@
 class Screenshot < ApplicationRecord
   has_many :detections
 
+  # 類似画像を探す範囲(分)
+  TIME_TO_TRACE = 5
+
+  # 同じ画像とみなす画像の類似度
+  MEAN_ERROR_PER_PIXEL = 5000
+
   def name
     self.src + "." + self.extension
   end
@@ -11,6 +17,19 @@ class Screenshot < ApplicationRecord
 
   # 5分前 ~ 1秒前 のスクリーンショットを1件取得
   def recent
-    Screenshot.where(created_at: (self.created_at - 5.minute)..(self.created_at - 1.second)).order("created_at desc").first
+    Screenshot.where(created_at: (self.created_at - TIME_TO_TRACE.minute)..(self.created_at - 1.second)).order("created_at desc").first
+  end
+
+  def error_per_pixel
+    if self.recent
+      img1 = Magick::Image.read(Rails.root.join(App::Application.config.screenshots_path, "test", self.name)).first
+      img2 = Magick::Image.read(Rails.root.join(App::Application.config.screenshots_path, "test", self.recent.name)).first
+      return img1.difference(img2)[0]
+    end
+    false
+  end
+
+  def similar?
+    self.error_per_pixel && self.error_per_pixel < MEAN_ERROR_PER_PIXEL
   end
 end
