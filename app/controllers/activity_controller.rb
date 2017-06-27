958729394a0c8e6bf5f762f403727989081d44f4
uenoryo <r.r.ag0611@outlook.jp>
2017-06-28 07:37:02 +0900

    Show activity logs in secret page.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,7 +1,10 @@
 class ActivityController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    return if params[:keyword] == User.find(params[:user_id]).keyword
-    render 'auth' if not @current_id or params[:user_id] != @current_user.id
+    @user = User.find(params[:user_id])
+    @the_day = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    @activities = @user.activities.aggregate @the_day
+    return if params[:keyword] == @user.keyword
+    render 'auth' if not current_user && params[:user_id] != current_user.id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :projects
+  has_many :activities
   before_save :set_keyword
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
