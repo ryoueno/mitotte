421fc5e67ec578998c70ece727b106f8f9ce4f26
uenoryo <r.r.ag0611@outlook.jp>
2017-07-11 15:58:26 +0900

    [wip] Implement drawing graph using gruff.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,49 @@ class Project < ApplicationRecord
   belongs_to :user
   validates :subject, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { maximum: 200 }
+  MAX_GRUFF_PERIOD = 10
+
+  def gruff_period
+    period = []
+    (self.start_at..self.end_at).each do |date|
+      period.push date
+    end
+    if period.count > MAX_GRUFF_PERIOD
+      div = period.count / MAX_GRUFF_PERIOD
+      data = period
+      period = []
+      MAX_GRUFF_PERIOD.times do |t|
+        period[t] = data[t * div]
+      end
+      period[0] = self.start_at
+      period[period.count - 1] = self.end_at
+    end
+    period
+  end
+
+  def all_tasl_num
+    self.tasks.count
+  end
+
+  def progress
+    prg = []
+    prg[0] = 0
+    gruff_period.each_with_index do |t, idx|
+      #prg[idx + 1] = idx * 200
+      prg[idx + 1] = idx * 200
+    end
+    #prg = [0, 20, 100, 100, 120, 330, 1000]
+    prg
+  end
+
+  def get_label
+    label = {}
+    label[0] = ''
+    gruff_period.each_with_index do |t, idx|
+      label[idx + 1] = t.strftime("%m/%d")
+    end
+    label
+  end
+
+
 end
