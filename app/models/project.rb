e07dd23edd5fb95d5957d841a61ad51cf69218b8
uenoryo <r.r.ag0611@outlook.jp>
2017-07-17 22:42:18 +0900

    Tasks belongs to projects are now countable.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,14 @@ class Project < ApplicationRecord
   belongs_to :user
   validates :subject, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { maximum: 200 }
+
   MAX_GRUFF_PERIOD = 10
+
+  TODO_STATUS = [
+    :INITIAL,
+    :PENNDING,
+    :PROGLESS,
+  ]
 
   def gruff_period
     period = []
@@ -23,8 +30,12 @@ class Project < ApplicationRecord
     period
   end
 
-  def all_tasl_num
+  def all_task_num
     self.tasks.count
+  end
+
+  def todo_task_num
+    tasks.where(:status => TaskStatus::STATUS.values_at(*TODO_STATUS)).count
   end
 
   def progress
