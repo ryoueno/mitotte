a0414e45130a061b17cbd0d30ec1338db16fa1ba
uenoryo <r.r.ag0611@outlook.jp>
2017-09-11 17:00:39 +0900

    Set up project model test.

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  it 'has a valid factory'
+
+  it 'is available the number of tasks'
+
+  it 'is available the number of tasks in the state of TODO'
+
+  it 'is available the values indicative progress'
+
+  it 'is available the label data used progress graph'
+
+  it 'is available time in hours'
+
+  it 'is available time in minutes'
+
+  it 'is available time in seconds'
+
+  it 'make a judgment to do now'
+end
