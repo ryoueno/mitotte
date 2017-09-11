3b39d4f00b812237fabcf4b1ccd764fd30319388
uenoryo <r.r.ag0611@outlook.jp>
2017-09-11 23:12:16 +0900

    Add project's label test.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -25,7 +25,41 @@ RSpec.describe Project, type: :model do
 
   it 'is available the values indicative progress'
 
-  it 'is available the label data used progress graph'
+  context 'divide project date for progress graph label' do
+    it 'is equal start-date and first period' do
+      project = create(:project)
+      expect(project.gruff_period.first).to eq project.start_at
+    end
+
+    it 'is equal end-date and last period if number of period is more than project days' do
+      project = create(:project)
+      max_period = 10
+      expect(project.gruff_period(max_period).last).to eq project.end_at
+    end
+
+    it 'is equal number of period and specified max_period' do
+      project = create(:project)
+      max_period = 5
+      expect(project.gruff_period(max_period).length).to eq max_period
+    end
+  end
+
+  context 'get label for progress graph divided periods' do
+    it 'is equal number of available periods and labels' do
+      project = create(:project)
+      blank_num = 1
+      expect(project.get_label.length - blank_num).to eq project.gruff_period.length
+    end
+
+    it 'is available label in specific format' do
+      project = create(:project)
+      expect(project.get_label[1]).to eq project.start_at.strftime("%m/%d")
+    end
+  end
+
+  it 'is available number of days' do
+    expect(create(:project).days).to eq 10
+  end
 
   it 'is available time in hours' do
     expect(create(:project).hours).to eq 8
