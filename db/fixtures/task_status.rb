98b5c3a1286e6d2ecb7b2a2a1debd3b7282ce7f7
uenoryo <r.r.ag0611@outlook.jp>
2017-09-18 14:20:16 +0900

    Modify task status from original class to model.

---
+++ db/fixtures/task_status.rb
@@ -0,0 +1,20 @@
+TaskStatus.seed do |s|
+  s.name = 'INITIAL'
+  s.display = '初期'
+end
+TaskStatus.seed do |s|
+  s.name = 'PENNDING'
+  s.display = 'しぶい'
+end
+TaskStatus.seed do |s|
+  s.name = 'PROGLESS'
+  s.display = '進行中'
+end
+TaskStatus.seed do |s|
+  s.name = 'DONE'
+  s.display = '完了'
+end
+TaskStatus.seed do |s|
+  s.name = 'REJECTED'
+  s.display = '却下'
+end
