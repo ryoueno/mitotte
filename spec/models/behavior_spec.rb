8e4bc12f8a764db8e7d2fa02d5ffe11b7ce4a115
uenoryo <r.r.ag0611@outlook.jp>
2017-10-05 17:21:50 +0900

    Replace activity factory to seeding.

--- spec/models/behavior_spec.rb
+++ spec/models/behavior_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Behavior, type: :model do
-  it 'has a valid factory' do
-    expect(build(:behavior)).to be_valid
+  context 'get the first record' do
+    before { @behavior = Behavior.first }
+    it { expect(@behavior.id).to be_a_kind_of(Integer) }
+    it { expect(@behavior.name).to be_a_kind_of(String) }
+    it { expect(@behavior.display).to be_a_kind_of(String) }
+    it { expect(@behavior.color_code.length).to eq 6 }
+    it { expect(@behavior.priority).to be_a_kind_of(Integer) }
   end
 end
