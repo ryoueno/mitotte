b6f81dae9fa4660178e582cf79a6e0da641bb18c
uenoryo <r.r.ag0611@outlook.jp>
2017-10-05 18:48:09 +0900

    Add where_behavior scope test.

--- db/fixtures/test/2017092700000005_activity.rb
+++ db/fixtures/test/2017092700000005_activity.rb
@@ -4,7 +4,42 @@ behavior_ids = Hash[Behavior.all.map { |row| [row.name, row.id] }]
 # Task status id list
 task_status_ids = Hash[TaskStatus.all.map { |row| [row.name, row.id] }]
 
-# CHANGE_STATUS
+### RESTING
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['RESTING']
+  s.created_at = 1.hours.ago
+end
+
+### WORKING
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['WORKING']
+  s.created_at = 2.hours.ago
+end
+
+### MOVING
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['MOVING']
+  s.created_at = 3.hours.ago
+end
+
+### LAZY
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['LAZY']
+  s.created_at = 4.hours.ago
+end
+
+### RUNNING
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['RUNNING']
+  s.created_at = 5.hours.ago
+end
+
+### CHANGE_STATUS
 # Task:1 | INITIAL => PENDING => PROGLESS => DONE
 Activity.seed do |s|
   s.user_id = 1
@@ -69,4 +104,11 @@ Activity.seed do |s|
   s.update_from = task_status_ids['INITIAL']
   s.update_to = task_status_ids['REJECTED']
   s.created_at = 1.days.ago
+end
+
+### DO_SOMETHING
+Activity.seed do |s|
+  s.user_id = 1
+  s.behavior_id = behavior_ids['DO_SOMETHING']
+  s.created_at = 6.hours.ago
 end
